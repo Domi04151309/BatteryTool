d41ba52ae8d782532a9a9a3bb15167620a55177f
Domi04151309 <domi04151309business@gmail.com>
2020-10-14 14:14:11 +0200

    Improve app selection

--- app/src/main/java/io/github/domi04151309/batterytool/activities/AddingActivity.kt
+++ app/src/main/java/io/github/domi04151309/batterytool/activities/AddingActivity.kt
@@ -1,10 +1,14 @@
 package io.github.domi04151309.batterytool.activities
 
+import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
+import android.graphics.drawable.LayerDrawable
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.preference.Preference
+import androidx.preference.PreferenceCategory
 import androidx.preference.PreferenceFragmentCompat
 import io.github.domi04151309.batterytool.R
 import io.github.domi04151309.batterytool.helpers.Theme
@@ -22,7 +26,10 @@ class AddingActivity : AppCompatActivity(),
             .commit()
     }
 
-    override fun onPreferenceStartFragment(caller: PreferenceFragmentCompat, pref: Preference): Boolean {
+    override fun onPreferenceStartFragment(
+        caller: PreferenceFragmentCompat,
+        pref: Preference
+    ): Boolean {
         val fragment = supportFragmentManager.fragmentFactory.instantiate(
             classLoader,
             pref.fragment
@@ -41,39 +48,85 @@ class AddingActivity : AppCompatActivity(),
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             addPreferencesFromResource(R.xml.pref_adding)
 
+            val pm: PackageManager = requireContext().packageManager
             val addingArray: ArrayList<CharSequence> = arrayListOf()
             val addingArrayDisplay: ArrayList<CharSequence> = arrayListOf()
             val bottomBar = requireActivity().findViewById<TextView>(R.id.bottom_title)
 
             Thread {
-                val pm: PackageManager = requireContext().packageManager
                 val packages = pm.getInstalledApplications(PackageManager.GET_META_DATA)
                 val arrayList: ArrayList<Preference> = ArrayList(packages.size)
+                val arrayListSystem: ArrayList<Preference> = ArrayList(packages.size)
                 for (packageInfo in packages) {
                     if (pm.getLaunchIntentForPackage(packageInfo.packageName) != null
-                        && packageInfo.packageName != requireContext().packageName) {
+                        && packageInfo.packageName != requireContext().packageName
+                    ) {
                         val preference = Preference(requireContext())
                         preference.icon = packageInfo.loadIcon(pm)
                         preference.title = packageInfo.loadLabel(pm)
                         preference.summary = packageInfo.packageName
                         preference.setOnPreferenceClickListener {
                             if (addingArray.contains(it.summary)) {
+                                it.icon = pm.getApplicationIcon(
+                                    pm.getApplicationInfo(
+                                        it.summary.toString(),
+                                        PackageManager.GET_META_DATA
+                                    )
+                                )
                                 addingArray.remove(it.summary)
-                                addingArrayDisplay.remove(pm.getApplicationLabel(pm.getApplicationInfo(it.summary.toString(), 0)))
-                            }
-                            else {
+                                addingArrayDisplay.remove(
+                                    pm.getApplicationLabel(
+                                        pm.getApplicationInfo(
+                                            it.summary.toString(),
+                                            PackageManager.GET_META_DATA
+                                        )
+                                    )
+                                )
+                            } else {
+                                it.icon = LayerDrawable(
+                                    arrayOf(
+                                        pm.getApplicationIcon(
+                                            pm.getApplicationInfo(
+                                                it.summary.toString(),
+                                                PackageManager.GET_META_DATA
+                                            )
+                                        ),
+                                        ContextCompat.getDrawable(
+                                            requireContext(),
+                                            R.drawable.overlay_icon
+                                        )
+                                    )
+                                )
                                 addingArray.add(it.summary)
-                                addingArrayDisplay.add(pm.getApplicationLabel(pm.getApplicationInfo(it.summary.toString(), 0)))
+                                addingArrayDisplay.add(
+                                    pm.getApplicationLabel(
+                                        pm.getApplicationInfo(
+                                            it.summary.toString(),
+                                            0
+                                        )
+                                    )
+                                )
                             }
                             bottomBar.text = addingArrayDisplay.joinToString()
                             true
                         }
-                        arrayList.add(preference)
+                        if (packageInfo.flags and ApplicationInfo.FLAG_SYSTEM != 0) {
+                            arrayListSystem.add(preference)
+                        } else {
+                            arrayList.add(preference)
+                        }
                     }
                 }
                 for (preference in arrayList.sortedWith(compareBy { it.title.toString() })) {
-                    preferenceScreen.addPreference(preference)
+                    findPreference<PreferenceCategory>("user")?.addPreference(preference)
                 }
+                for (preference in arrayListSystem.sortedWith(compareBy { it.title.toString() })) {
+                    findPreference<PreferenceCategory>("system")?.addPreference(preference)
+                }
+                val bottomDivider = Preference(requireContext())
+                bottomDivider.layoutResource = R.layout.preference_divider
+                bottomDivider.isSelectable = false
+                preferenceScreen.addPreference(bottomDivider)
             }.start()
         }
     }
