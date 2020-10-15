a42b2642cf9a09c9c9641d2f448c6daa554f2245
Domi04151309 <domi04151309business@gmail.com>
2020-10-15 19:39:59 +0200

    Add quick tile

--- app/src/main/java/io/github/domi04151309/batterytool/receivers/StartReceiver.kt
+++ app/src/main/java/io/github/domi04151309/batterytool/receivers/StartReceiver.kt
@@ -1,10 +1,14 @@
 package io.github.domi04151309.batterytool.receivers
 
 import android.content.BroadcastReceiver
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.service.quicksettings.TileService
 import androidx.core.content.ContextCompat
 import io.github.domi04151309.batterytool.services.ForegroundService
+import io.github.domi04151309.batterytool.services.QuickTileService
 
 class StartReceiver : BroadcastReceiver() {
 
@@ -14,6 +18,12 @@ class StartReceiver : BroadcastReceiver() {
                 context,
                 Intent(context, ForegroundService::class.java)
             )
+            if (Build.VERSION.SDK_INT >= 24) {
+                TileService.requestListeningState(
+                    context,
+                    ComponentName(context, QuickTileService::class.java)
+                )
+            }
         }
     }
 }
---
+++ app/src/main/java/io/github/domi04151309/batterytool/services/QuickTileService.kt
@@ -0,0 +1,17 @@
+package io.github.domi04151309.batterytool.services
+
+import android.annotation.TargetApi
+import android.os.Build
+import android.service.quicksettings.TileService
+import android.widget.Toast
+import io.github.domi04151309.batterytool.R
+import io.github.domi04151309.batterytool.helpers.AppHelper
+
+@TargetApi(Build.VERSION_CODES.N)
+class QuickTileService : TileService() {
+
+    override fun onClick() {
+        AppHelper.hibernate(this)
+        Toast.makeText(this, R.string.toast_stopped_all, Toast.LENGTH_SHORT).show()
+    }
+}
