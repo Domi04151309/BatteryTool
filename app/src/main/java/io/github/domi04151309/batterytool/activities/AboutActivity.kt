b56b04da6a02c91ff0df73d93a7176983906d26e
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2022-04-24 16:30:37 +0200

    Update about activity

--- app/src/main/java/io/github/domi04151309/batterytool/activities/AboutActivity.kt
+++ app/src/main/java/io/github/domi04151309/batterytool/activities/AboutActivity.kt
@@ -1,26 +1,114 @@
 package io.github.domi04151309.batterytool.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.text.method.LinkMovementMethod
-import android.widget.TextView
-import io.github.domi04151309.batterytool.BuildConfig
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.pm.PackageInfoCompat
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
 import io.github.domi04151309.batterytool.R
 import io.github.domi04151309.batterytool.helpers.Theme
 
 class AboutActivity : AppCompatActivity() {
 
+    companion object {
+        internal const val GITHUB_REPOSITORY: String = "Domi04151309/BatteryTool"
+        private const val REPOSITORY_URL: String = "https://github.com/$GITHUB_REPOSITORY"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         Theme.set(this)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about)
-        findViewById<TextView>(R.id.versionTxt).text = getString(
-            R.string.about_version,
-            BuildConfig.VERSION_NAME
-        )
-        findViewById<TextView>(R.id.by).movementMethod = LinkMovementMethod.getInstance()
-        findViewById<TextView>(R.id.github).movementMethod = LinkMovementMethod.getInstance()
-        findViewById<TextView>(R.id.license).movementMethod = LinkMovementMethod.getInstance()
-        findViewById<TextView>(R.id.icons).movementMethod = LinkMovementMethod.getInstance()
+        setContentView(R.layout.activity_settings)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.settings, GeneralPreferenceFragment())
+            .commit()
+    }
+
+    class GeneralPreferenceFragment : PreferenceFragmentCompat() {
+
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            addPreferencesFromResource(R.xml.pref_about)
+            findPreference<Preference>("app_version")?.apply {
+                val pInfo =
+                    requireContext().packageManager.getPackageInfo(requireContext().packageName, 0)
+                summary = requireContext().getString(
+                    R.string.about_app_version_desc,
+                    pInfo.versionName,
+                    PackageInfoCompat.getLongVersionCode(pInfo)
+                )
+                setOnPreferenceClickListener {
+                    startActivity(
+                        Intent(
+                            Intent.ACTION_VIEW,
+                            Uri.parse("$REPOSITORY_URL/releases")
+                        )
+                    )
+                    true
+                }
+            }
+            findPreference<Preference>("github")?.apply {
+                summary = REPOSITORY_URL
+                setOnPreferenceClickListener {
+                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(REPOSITORY_URL)))
+                    true
+                }
+            }
+            findPreference<Preference>("license")?.setOnPreferenceClickListener {
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("$REPOSITORY_URL/blob/master/LICENSE")
+                    )
+                )
+                true
+            }
+            findPreference<Preference>("icons")?.setOnPreferenceClickListener {
+                AlertDialog.Builder(requireContext())
+                    .setTitle(R.string.about_icons)
+                    .setItems(resources.getStringArray(R.array.about_icons_array)) { _, which ->
+                        startActivity(
+                            Intent(
+                                Intent.ACTION_VIEW, Uri.parse(
+                                    when (which) {
+                                        0 -> "https://icons8.com/"
+                                        1 -> "https://fonts.google.com/icons?selected=Material+Icons"
+                                        else -> "about:blank"
+                                    }
+                                )
+                            )
+                        )
+                    }
+                    .show()
+                true
+            }
+            findPreference<Preference>("contributors")?.setOnPreferenceClickListener {
+                AlertDialog.Builder(requireContext())
+                    .setTitle(R.string.about_privacy)
+                    .setMessage(R.string.about_privacy_desc)
+                    .setPositiveButton(android.R.string.ok) { _, _ ->
+                        startActivity(Intent(requireContext(), ContributorActivity::class.java))
+                    }
+                    .setNegativeButton(android.R.string.cancel) { _, _ -> }
+                    .setNeutralButton(R.string.about_privacy_policy) { _, _ ->
+                        startActivity(
+                            Intent(
+                                Intent.ACTION_VIEW, Uri.parse(
+                                    "https://docs.github.com/en/github/site-policy/github-privacy-statement"
+                                )
+                            )
+                        )
+                    }
+                    .show()
+                true
+            }
+            findPreference<Preference>("libraries")?.setOnPreferenceClickListener {
+                startActivity(Intent(requireContext(), LibraryActivity::class.java))
+                true
+            }
+        }
     }
 }
---
+++ app/src/main/java/io/github/domi04151309/batterytool/helpers/Global.kt
@@ -0,0 +1,5 @@
+package io.github.domi04151309.batterytool.helpers
+
+internal object Global {
+    const val LOG_TAG: String = "BatteryTool"
+}
