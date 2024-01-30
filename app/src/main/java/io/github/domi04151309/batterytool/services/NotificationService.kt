02c58ba692cb207b818acbc2a82c61a9e7a7d572
domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2024-01-30 17:21:51 +0100

    Update NotificationService.kt

--- app/src/main/java/io/github/domi04151309/batterytool/services/NotificationService.kt
+++ app/src/main/java/io/github/domi04151309/batterytool/services/NotificationService.kt
@@ -24,69 +24,67 @@ class NotificationService : NotificationListenerService() {
         startId: Int,
     ): Int = Service.START_STICKY
 
-    override fun onListenerConnected() {
-        super.onListenerConnected()
-        instance = WeakReference(this)
-    }
-
     private fun getNotifications(): List<StatusBarNotification> =
         try {
             activeNotifications.sortedBy { it.postTime }
-        } catch (e: SecurityException) {
-            Log.w(Global.LOG_TAG, e)
+        } catch (exception: SecurityException) {
+            Log.w(Global.LOG_TAG, exception)
             emptyList()
         }
 
     fun getPlayingPackageName(callback: (String?) -> Unit) {
         try {
-            val notifications =
+            val notification =
                 getNotifications().filter {
                     it.notification.category == Notification.CATEGORY_TRANSPORT ||
                         it.notification.category == Notification.CATEGORY_SERVICE
-                }
-            val notification =
-                notifications.findLast {
+                }.findLast {
                     it.notification.extras[NotificationCompat.EXTRA_MEDIA_SESSION] as? MediaSession.Token != null
                 }
-            if (notification != null) {
-                val token =
-                    notification.notification.extras[NotificationCompat.EXTRA_MEDIA_SESSION] as? MediaSession.Token
-                var mediaController: MediaController? = null
-                val mediaSessionCallback =
-                    object : MediaController.ControllerCallback() {
-                        override fun onConnected(
-                            controller: MediaController,
-                            allowedCommands: SessionCommandGroup,
-                        ) {
-                            super.onConnected(controller, allowedCommands)
-                            if (controller != mediaController) return
-                            if (controller.playerState == SessionPlayer.PLAYER_STATE_PLAYING) {
-                                callback(notification.packageName)
-                            } else {
-                                callback(null)
-                            }
-                            try {
-                                mediaController?.close()
-                            } catch (e: IOException) {
-                                Log.w(Global.LOG_TAG, e)
-                            }
+            if (notification == null) {
+                callback(null)
+                return
+            }
+            val token =
+                notification.notification.extras[NotificationCompat.EXTRA_MEDIA_SESSION] as? MediaSession.Token
+            var mediaController: MediaController? = null
+            val mediaSessionCallback =
+                object : MediaController.ControllerCallback() {
+                    override fun onConnected(
+                        controller: MediaController,
+                        allowedCommands: SessionCommandGroup,
+                    ) {
+                        super.onConnected(controller, allowedCommands)
+                        if (controller != mediaController) return
+                        if (controller.playerState == SessionPlayer.PLAYER_STATE_PLAYING) {
+                            callback(notification.packageName)
+                        } else {
+                            callback(null)
+                        }
+                        try {
+                            mediaController?.close()
+                        } catch (exception: IOException) {
+                            Log.w(Global.LOG_TAG, exception)
                         }
                     }
-                mediaController =
-                    MediaController.Builder(this)
-                        .setSessionCompatToken(MediaSessionCompat.Token.fromToken(token))
-                        .setControllerCallback(
-                            Executors.newSingleThreadExecutor(),
-                            mediaSessionCallback,
-                        )
-                        .build()
-            } else {
-                callback(null)
-            }
-        } catch (e: SecurityException) {
-            Log.w(Global.LOG_TAG, e)
+                }
+            mediaController =
+                MediaController.Builder(this)
+                    .setSessionCompatToken(MediaSessionCompat.Token.fromToken(token))
+                    .setControllerCallback(
+                        Executors.newSingleThreadExecutor(),
+                        mediaSessionCallback,
+                    )
+                    .build()
+        } catch (exception: SecurityException) {
+            Log.w(Global.LOG_TAG, exception)
             callback(null)
         }
+    }
+
+    override fun onListenerConnected() {
+        super.onListenerConnected()
+        instance = WeakReference(this)
     }
 
     override fun onListenerDisconnected() {
