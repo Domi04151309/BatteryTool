3afe1c6d0ae20e124e6297aa07775c2c0410f65b
domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2024-02-12 03:24:20 +0100

    Change pseudo hash set base

--- app/src/main/java/io/github/domi04151309/batterytool/helpers/PseudoHashSet.kt
+++ app/src/main/java/io/github/domi04151309/batterytool/helpers/PseudoHashSet.kt
@@ -1,11 +1,14 @@
 package io.github.domi04151309.batterytool.helpers
 
-class PseudoHashSet : HashSet<String>() {
-    @Suppress("SameReturnValue")
-    override fun contains(element: String): Boolean = false
+class PseudoHashSet : Set<String> {
+    override val size: Int
+        get() = 0
 
-    companion object {
-        @java.io.Serial
-        private const val serialVersionUID = 1L
-    }
+    override fun isEmpty(): Boolean = true
+
+    override fun iterator(): Iterator<String> = setOf<String>().iterator()
+
+    override fun containsAll(elements: Collection<String>): Boolean = true
+
+    override fun contains(element: String): Boolean = true
 }
